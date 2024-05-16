e1948841c57ad39cc70b12236fa5ce0bf9cf2419
HarleyR2023 <U21313032@utp.edu.pe>
2024-05-16 18:01:55 -0500

    cambiar el puerto :v

--- src/main/java/com/utp/modamovil/BaseDatos.java
+++ src/main/java/com/utp/modamovil/BaseDatos.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 public class BaseDatos {
     public static final BaseDatos INSTANCE = new BaseDatos();
     
-    private static final String URL = "jdbc:mysql://localhost:3309/modamovil";
+    private static final String URL = "jdbc:mysql://localhost:3306/modamovil";
     private static final String USER = "root";
     private static final String PASSWORD = "";
     
